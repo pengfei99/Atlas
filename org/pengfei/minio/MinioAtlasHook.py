2a939b55b6e8183414365ba9e5f003d2a63bbfca
pengfei <liu.pengfei@hotmail.fr>
2020-10-16 12:06:02 +0200

    complete hook and bug fix

--- org/pengfei/minio/MinioAtlasHook.py
+++ org/pengfei/minio/MinioAtlasHook.py
@@ -5,6 +5,7 @@ from org.pengfei.entity_management.s3.S3BucketManager import S3BucketManager
 from org.pengfei.entity_management.s3.S3PsDirManager import S3PsDirManager
 from org.pengfei.entity_management.s3.S3ObjectManager import S3ObjectManager
 from datetime import datetime
+import os
 
 
 class MinioAtlasHook:
@@ -64,16 +65,27 @@ class MinioAtlasHook:
                                              bucket_description, create_time=create_time_stamp)
 
     def create_atlas_ps_dir(self, ps_dir_metadata, ps_dir_description):
-            bucket_name = ps_dir_metadata['name'].split("/")[0]
-            entity_name = ps_dir_metadata['name'].split("/")[-1]
-            qualified_bucket_name = self.minio_end_point + "/" + bucket_name
-            qualified_entity_name = qualified_bucket_name + "/" + entity_name
-            prefix = entity_name + "/"
-            self.s3_ps_dir_manager.create_entity(entity_name, qualified_entity_name,
-                                                 qualified_bucket_name, prefix, description=ps_dir_description)
+        names = ps_dir_metadata['name'].split("/")
+        bucket_name = names[0]
+        entity_name = "/".join(names[1:])
+        qualified_bucket_name = self.minio_end_point + "/" + bucket_name
+        qualified_entity_name = qualified_bucket_name + "/" + entity_name
+        prefix = entity_name + "/"
+        self.s3_ps_dir_manager.create_entity(entity_name, qualified_entity_name,
+                                             qualified_bucket_name, prefix, description=ps_dir_description)
 
     def create_atlas_object(self, object_metadata, object_description):
-            self.s3_obj_manager.create_entity("toto.csv", "s3://pengfei.org/pengfei_test1/data_science/toto.csv",
-                                              "s3://pengfei.org/pengfei_test1/data_science", "data_science", "csv",
-                                              "pengfei",
-                                              "test txt")
+        names = object_metadata['name'].split('/')
+        entity_name = names[-1]
+        qualified_entity_name = object_metadata['name']
+        qualified_ps_dir_name = "/".join(names[:-1])
+        ps_dir_prefix = "/".join(names[1:-1]) + "/"
+        extension = str(os.path.splitext(entity_name)[1])[1:]
+        date = object_metadata['LastModified']
+        last_modified_stamp = round(datetime.timestamp(date) * 1000)
+        size = object_metadata['size']
+        self.s3_object_manager.create_entity(entity_name, qualified_entity_name,
+                                             qualified_ps_dir_name, ps_dir_prefix, extension,
+                                             "pengfei",
+                                             object_description, create_time=last_modified_stamp,
+                                             update_time=last_modified_stamp, size=size)
---
+++ org/pengfei/minio/MinioScanner.py
@@ -0,0 +1,2 @@
+def dir_scanner(self, path):
+    self.fs.ls(path)
